9ef74a3687a93632320262aa29de7196e0444f6e
Daniella Syombua <syombuadaniella@gmail.com>
2025-06-14 09:18:19 +0300

    Adding two parts

---
+++ index.js
@@ -0,0 +1,13 @@
+document.getElementById("content").style.display="none";
+        var flag =1;
+        function faqsquiz() {
+        if(flag==1)
+        {
+        document.getElementById("content").style.display="block"; 
+        flag=0;
+        }
+        else{
+        document.getElementById("content").style.display="none"; 
+        flag=1;
+  }
+};
