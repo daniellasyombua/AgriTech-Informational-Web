7ad2aa42002ffd1f235a77b61022ff248340304f
BerissaMuyizere <muyizereberissa@gmail.com>
2025-06-15 12:21:04 +0300

    what they say about us

--- index.js
+++ index.js
@@ -11,3 +11,15 @@ document.getElementById("content").style.display="none";
         flag=1;
   }
 };
+
+const wrapper = document.getElementById('testimonialWrapper');
+const nextBtn = document.getElementById('nextBtn');
+const prevBtn = document.getElementById('prevBtn');
+
+let scrollAmount = 0;
+nextBtn.addEventListener('click', () => {
+  wrapper.scrollBy({ left: 270, behavior: 'smooth' });
+});
+prevBtn.addEventListener('click', () => {
+  wrapper.scrollBy({ left: -270, behavior: 'smooth' });
+});
